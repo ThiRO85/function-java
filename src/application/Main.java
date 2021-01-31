0acdfc28b5f0144f4bff49b7e05613d7e6c4f784
Thiago Rodrigues <thgr85@gmail.com>
2021-01-31 14:24:55 -0200

    Expressao lambda declarada

--- src/application/Main.java
+++ src/application/Main.java
@@ -5,6 +5,7 @@ import model.entities.Product;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Locale;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Main {
@@ -19,7 +20,9 @@ public class Main {
         list.add(new Product("Tablet", 350.00));
         list.add(new Product("HD Case", 80.90));
 
-        List<String> names = list.stream().map(Product::nonStaticUpperCaseName).collect(Collectors.toList());
+        Function<Product, String> func = p -> p.getName().toUpperCase();
+
+        List<String> names = list.stream().map(func).collect(Collectors.toList());
 
         names.forEach(System.out::println);
     }
